9deb798d3b92223da3c66ae732fd7f4472b4055e
adijadia786 <62186740+adijadia786@users.noreply.github.com>
2020-03-15 01:11:08 +0530

    Update election_problem.sql

--- election_problem.sql
+++ election_problem.sql
@@ -1,3 +1,11 @@
+/*
+# Consider a scenario where we have 4 parties and a person can give votes 
+to multiple parties but if he/she votes multiple time so the vote should be adjusted accordingly. 
+Like a person votes all the 4 parties so his vote goes to each party by 0.25,
+if votes thrice so vote should go like 0.33. How you will handle this, how many tables??
+
+*/
+-- 3 tables, 2 dims and 1 fact
 select party_name from (
 with trans as
 (
